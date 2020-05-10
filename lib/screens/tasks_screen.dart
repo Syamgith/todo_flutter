b56d448e55839d73a18172094dedbc31ba9da152
syamgith <syamgithksudheesh@gmail.com>
2020-05-10 17:49:01 +0530

    icon and text

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xfff052019),
+      body: Container(
+        padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(Icons.list),
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+            ),
+            Text(
+              'To Do',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
